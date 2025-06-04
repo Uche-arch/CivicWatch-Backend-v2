b846c7a1bf608ada3582abcc5bc089a3150fba74
Uche Godswill <auchjee@gmail.com>
2025-06-04 01:24:53 +0100

    Update email template

--- utils/email.js
+++ utils/email.js
@@ -19,11 +19,15 @@ const sendResetEmail = async (to, resetLink) => {
   await transporter.sendMail({
     from: `"CrimeMap Support" <${process.env.EMAIL_USER}>`,
     to,
-    subject: "Reset your CrimeMap password",
+    subject: "Reset your CivicWatch password",
     html: `
-      <p>You requested a password reset.</p>
-      <p>Click <a href="${resetLink}">here</a> to reset your password.</p>
-      <p>This link will expire in 15 minutes.</p>
+      <p>Hello ${user.username},</p>
+      <p>We received a request to reset your password for your CivicWatch account.</p>
+      <p>If you made this request, you can reset your password by clicking the link below:</p>
+      <p><a href="${resetLink}">Reset your password</a></p>
+      <p><strong>This link will expire in 15 minutes.</strong></p>
+      <p>If you did <strong>not</strong> request a password reset, please ignore this email. No changes will be made to your account.</p>
+      <p>Stay safe,<br>The CivicWatch Team</p>
     `,
   });
   console.log("✅ Reset email sent to", to);
