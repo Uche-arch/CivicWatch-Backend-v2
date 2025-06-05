0265295076bca83308a115e8c5609f0efafeaa98
Uche Godswill <auchjee@gmail.com>
2025-06-05 10:01:50 +0100

    Update platform name

--- utils/email.js
+++ utils/email.js
@@ -17,17 +17,17 @@ const sendResetEmail = async (to, resetLink) => {
   });
 
   await transporter.sendMail({
-    from: `"CivicWatch Support" <${process.env.EMAIL_USER}>`,
+    from: `"Phylax360 Support" <${process.env.EMAIL_USER}>`,
     to,
-    subject: "Reset your CivicWatch password",
+    subject: "Reset your Phylax360 account password",
     html: `
       <p>Hello,</p>
-      <p>We received a request to reset your password for your CivicWatch account.</p>
+      <p>We received a request to reset your password for your Phylax360 account.</p>
       <p>If you made this request, you can reset your password by clicking the link below:</p>
       <p><a href="${resetLink}">Reset your password</a></p>
       <p><strong>This link will expire in 15 minutes.</strong></p>
       <p>If you did <strong>not</strong> request a password reset, please ignore this email. No changes will be made to your account.</p>
-      <p>Stay safe,<br>The CivicWatch Team</p>
+      <p>Stay safe,<br>The Phylax360 Team</p>
     `,
   });
   console.log("✅ Reset email sent to", to);
